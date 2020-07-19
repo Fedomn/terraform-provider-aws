a77583c630735b3ca0146aa736cca8b25d42a27f
Fedomn <shadowforfiend@gmail.com>
2020-07-19 22:54:43 +0800

    move all waiter functions and logic to internal rds package

--- aws/internal/service/rds/waiter/status.go
+++ aws/internal/service/rds/waiter/status.go
@@ -1,7 +1,13 @@
 package waiter
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strings"
+
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
@@ -33,4 +39,46 @@ func EventSubscriptionStatus(conn *rds.RDS, subscriptionName string) resource.St
 
 		return output.EventSubscriptionsList[0], aws.StringValue(output.EventSubscriptionsList[0].Status), nil
 	}
+}
+
+// ActivityStreamStatus fetches the RDS Cluster Activity Stream Status
+func ActivityStreamStatus(conn *rds.RDS, dbClusterIdentifier string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		emptyResp := &rds.DescribeDBClustersInput{}
+
+		resp, err := conn.DescribeDBClusters(&rds.DescribeDBClustersInput{
+			DBClusterIdentifier: aws.String(dbClusterIdentifier),
+		})
+
+		if err != nil {
+			log.Printf("[DEBUG] Refreshing RDS Cluster Activity Stream State. Occur error: %s", err)
+			if isAWSErr(err, rds.ErrCodeDBClusterNotFoundFault, "") {
+				return emptyResp, rds.ActivityStreamStatusStopped, nil
+			} else if resp != nil && len(resp.DBClusters) == 0 {
+				return emptyResp, rds.ActivityStreamStatusStopped, nil
+			} else {
+				return emptyResp, "", fmt.Errorf("error on refresh: %+v", err)
+			}
+		}
+
+		if resp == nil || resp.DBClusters == nil || len(resp.DBClusters) == 0 {
+			log.Printf("[DEBUG] Refreshing RDS Cluster Activity Stream State. Invalid resp: %s", resp)
+			return emptyResp, rds.ActivityStreamStatusStopped, nil
+		}
+
+		cluster := resp.DBClusters[0]
+		status := aws.StringValue(cluster.ActivityStreamStatus)
+		log.Printf("[DEBUG] Refreshing RDS Cluster Activity Stream State... %s", status)
+		return cluster, status, nil
+	}
+}
+
+// Similar to isAWSErr from aws/awserr.go
+// TODO: Add and export in shared package
+func isAWSErr(err error, code string, message string) bool {
+	var awsErr awserr.Error
+	if errors.As(err, &awsErr) {
+		return awsErr.Code() == code && strings.Contains(awsErr.Message(), message)
+	}
+	return false
 }
